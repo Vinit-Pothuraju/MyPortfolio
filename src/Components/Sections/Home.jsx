404b79bc379cc427324b76bdb28e520d96d2424c
Pothuraju Vinit Kumar <vinitkumarpothuraju@gmail.com>
2025-06-07 16:13:59 +0530

    Update Home.jsx

--- src/Components/Sections/Home.jsx
+++ src/Components/Sections/Home.jsx
@@ -13,7 +13,7 @@ const Home = ({showEmojis}) => {
       <RevealOnScroll>
         <div className="text-center z-10 relative px-4">
           <h1 className="text-5xl md:text-7xl font-bold mb-6  bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
-            Hi,I'm Vinit Kumar
+            Hi,I"m Vinit Kumar
           </h1>
           <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto ">
             Growing as a full stack developer by building, breaking, and
